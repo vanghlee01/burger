1f55080b5d4c87cf9cd5c364db771d3f12ee8ea8
Hleeeeee <vanghlee01@gmail.com>
2019-12-30 03:40:15 -0600

    pusshing final codes

---
+++ config/orm.js
@@ -0,0 +1,13 @@
+var connection = require("./connection.js");
+var orm = {
+    select: function(whatToselct, table)
+}
+//In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. 
+
+// selectAll()
+// insertOne()
+// updateOne()
+
+
+n 
+// Export the ORM object in module.exports.
--- server.js
+++ server.js
@@ -12,17 +12,17 @@ app.json(express.json());
 app.engine("handlebars", exphbs({ defaultLayout:"main"}));
 app.set("view.engine", "handlebars");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "",
+//     database: "burgers_db"
+// });
 
-connection.connect(function(err) {
-    if(err) {
-        console.error("")
-    }
-}
-app.get("/")
+// connection.connect(function(err) {
+//     if(err) {
+//         console.error("")
+//     }
+// }
+// app.get("/")
